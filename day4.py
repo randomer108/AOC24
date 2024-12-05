efa7b62a5aac70b165be3489de85f6c7ccab395b
randomer108 <jacob.gibbs@gmx.co.uk>
2024-12-05 08:37:42 +0000

    day4done

--- day4.py
+++ day4.py
@@ -1,17 +1,78 @@
 import re
 import pandas as pd
+import numpy as np
 
 data = list(open("./data/day4.txt"))
-data_rev = [line[::-1] for line in data]
-data_down = []
-for
+data = [re.sub(r'\n','',x) for x in data]
+data = np.array(data)
 
-def searchList(list):
-    finds = re.findall(r'XMAS',str(list))
-    return(len(finds))
+answer=0 
+for row,ROW in enumerate(data): 
+    ROW=str(ROW)
+    for col,element in enumerate(ROW):
+        if element == 'X':
+            # Right
+            if col<137:
+                if data[row][col+1]=='M' and data[row][col+2]=='A' and data[row][col+3]=='S':
+                    answer+=1
 
-answer=0
-for line in data:
-    answer +=searchList(line)
+            # Left
+            if col>2:
+                if data[row][col-1]=='M' and data[row][col-2]=='A' and data[row][col-3]=='S':
+                    answer+=1
+            
+            # Up
+            if row>2:
+                if data[row-1][col]=='M' and data[row-2][col]=='A' and data[row-3][col]=='S':
+                    answer+=1
+            # Down
+            if row<137:
+                if data[row+1][col]=='M' and data[row+2][col]=='A' and data[row+3][col]=='S':
+                    answer+=1
+            
+            #diag_right up
+            if col<137 and row>2:
+                if data[row-1][col+1]=='M' and data[row-2][col+2]=='A' and data[row-3][col+3]=='S':
+                    answer+=1
 
+             #diag right down
+            if col<137 and row<137:
+                if data[row+1][col+1]=='M' and data[row+2][col+2]=='A' and data[row+3][col+3]=='S':
+                    answer+=1
+
+            #diag left down
+            if col>2 and row<137:
+                if data[row+1][col-1]=='M' and data[row+2][col-2]=='A' and data[row+3][col-3]=='S':
+                    answer+=1
+
+            #diag left up
+            if col>2 and row>2:
+                if data[row-1][col-1]=='M' and data[row-2][col-2]=='A' and data[row-3][col-3]=='S':
+                    answer+=1
+print(answer)
+
+# 2
+answer=0 
+# Imagine picking an A and going clockwise from top right, collecting the corners around the A. You'll get 4 elements: i,j,k,l.
+# Only certain combos of i,j,k,l are valid, in that they will make an X-mas.
+# (pen and paper - note that the order needs to be MMSS or a "shift" of that like SMMS. Cannot be SMSM as that would spell {SAS + MAM} instead of {MAS/SAM + MAS/SAM})
+valid= [['M','M','S','S'],
+        ['M','S','S','M'],
+        ['S','S','M','M'],
+        ['S','M','M','S'],
+        ]
+
+for row,ROW in enumerate(data): 
+    for col,element in enumerate(ROW):
+        if element == 'A':
+            if col>0 and col<139 and row>0 and row<139:
+                # go clockwise from top right getting the corers around the A.
+                i = data[row-1][col+1]
+                j = data[row+1][col+1]
+                k = data[row+1][col-1]
+                l = data[row-1][col-1]
+                
+                if [i,j,k,l] in valid:
+                    answer+=1             #diag right down
+           
 print(answer)
